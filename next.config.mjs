5d94ccf4113af055148f976451abbdc793505e77
Dread <34528298+islandbitcoin@users.noreply.github.com>
2025-03-02 19:45:33 -0500

    Island bitcoin (#1)
    * changed redirects
    
    * changed redirects
    
    * changed redirects
    
    * added index.html
    
    * added index.html
    
    * added backgrounds
    
    * updated index.html
    
    * resized images
    
    * update www
    
    * updated
    
    * updated
    
    * updated
    
    * updated
    
    * updated
    
    * updated
    
    * updated
    
    * updated
    
    * updated
    
    * updated
    
    * updated
    
    * updated
    
    * added logging for websockets
    
    * add falmouth subdomain
    
    * updated
    
    * updated
    
    ---------
    
    Co-authored-by: D++ <adacountesslovelace@gmail.com>

--- next.config.mjs
+++ next.config.mjs
@@ -3,13 +3,48 @@ const nextConfig = {
   async rewrites() {
     return [
       {
-        source: '/.well-known/lnurlp/:user',
-        destination: '/api/lnurlp/:user',
-      }
-    ]
+        source: "/.well-known/lnurlp/:user",
+        destination: "/api/lnurlp/:user",
+      },
+      {
+        source: "/.well-known/nostr.json",
+        destination: "/api/nostr/name",
+      },
+    ];
   },
   async redirects() {
     return [
+      // Trezor Academy subdomains
+      {
+        source: "/",
+        destination: "https://trezoracademy.rsvpify.com",
+        has: [{ type: "host", value: "trezoracademy.islandbitcoin.com" }],
+        permanent: false,
+      },
+      {
+        source: "/",
+        destination: "https://falmouthbitcoinworkshop.rsvpify.com",
+        has: [{ type: "host", value: "falmouth.islandbitcoin.com" }],
+        permanent: false,
+      },
+      // Main homepage
+      {
+        source: "/",
+        destination: "/index.html",
+        permanent: true,
+      },
+      {
+        source: "/",
+        destination: "/community.html",
+        has: [{ type: "host", value: "www.islandbitcoin.com" }],
+        permanent: true,
+      },
+      {
+        source: "/",
+        destination: "/index.html",
+        has: [{ type: "host", value: "www.taddesse.xyz" }],
+        permanent: true,
+      },
     ];
   },
 };
